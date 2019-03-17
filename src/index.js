dd079dbbeb83b131d4144986dc5a783153b0552a
Kaifaat <artem.meka@gmail.com>
2019-03-17 16:37:23 +0300

    feat: add love-triangle task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    // your implementation
+    //[2, 3, 1, 5, 6, 4, 2, 3, 1, 5, 6, 4, 2, 3, 1, 5, 6, 4]
+
+    let countTriangles = 0;
+    for (let i = 0; i < preferences.length; i++) {
+        if ((i + 1) === preferences[preferences[preferences[i] - 1] - 1]) {
+            countTriangles++;
+        }
+    }
+    console.log(countTriangles);
+    return Math.floor(countTriangles/3);
 };
+
